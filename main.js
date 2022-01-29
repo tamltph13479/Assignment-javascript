d4531c89112a891134d32091326bc232c8372c76
Thetam007 <tamltph13479@fpt.edu.vn>
2022-01-29 22:28:12 +0700

    update

--- main.js
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-
-document.querySelector("#app").innerHTML = `
-  <h1>Hello le the ta</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
