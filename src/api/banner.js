941533e842bf2ae0ae892ce1fdc2ac82047c1d96
tamltph13479 <thetam1002@gmail.com>
2022-02-21 22:58:23 +0700

    banner

---
+++ src/api/banner.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+const slideImages = [
+    'images/slide_2.jpg',
+    'images/slide_3.jpg',
+    'images/slide_4.jpg'
+];
+
+const Slideshow = () => {
+    return ( <
+        div >
+        <
+        Slide easing = "ease" >
+        <
+        div className = "each-slide" >
+        <
+        div style = {
+            { 'backgroundImage': `url(${slideImages[0]})` }
+        } >
+        <
+        span > Slide 1 < /span> < /
+        div > <
+        /div> <
+        div className = "each-slide" >
+        <
+        div style = {
+            { 'backgroundImage': `url(${slideImages[1]})` }
+        } >
+        <
+        span > Slide 2 < /span> < /
+        div > <
+        /div> <
+        div className = "each-slide" >
+        <
+        div style = {
+            { 'backgroundImage': `url(${slideImages[2]})` }
+        } >
+        <
+        span > Slide 3 < /span> < /
+        div > <
+        /div> < /
+        Slide > <
+        /div>
+    )
+};
+
+export default Slideshow;
